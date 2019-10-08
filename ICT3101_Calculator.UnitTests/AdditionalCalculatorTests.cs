f29696143cc4322f7ff13f0115929dc85136bb05
Roy Lee <1700500@sit.singaporetech.edu.sg>
2019-10-08 19:50:48 +0800

    Completed Lab 4

--- ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
+++ ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ConsoleApp2;
+using Moq;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,24 @@ namespace ICT3101_Calculator.UnitTests
 {
     class AdditionalCalculatorTests
     {
+        private Calculator _calculator;
+        private Mock<IFileReader> _mockFileReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockFileReader = new Mock<IFileReader>();
+            _mockFileReader.Setup(fr =>
+                fr.Read("../../../../ConsoleApp2/MagicNumbers.txt")).Returns(new string[2] { "42", "42" });
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        [TestCase(1)]
+        public void MagicNumber_WithEachLine_ResultExpectedResult(double choice)
+        {
+            Assert.That(() => _calculator.GenMagicNum(choice, _mockFileReader.Object), Is.EqualTo(84));
+        }
+
     }
 }
